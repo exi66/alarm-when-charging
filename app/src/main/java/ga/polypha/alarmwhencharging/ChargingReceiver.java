a8e23cedad7045be354e6243d0c3672c446bc902
exi66 <eternityexi@gmail.com>
2023-04-21 02:07:59 +0400

    fix realese build issue

--- app/src/main/java/ga/polypha/alarmwhencharging/ChargingReceiver.java
+++ app/src/main/java/ga/polypha/alarmwhencharging/ChargingReceiver.java
@@ -6,19 +6,24 @@ import android.content.Intent;
 
 public class ChargingReceiver extends BroadcastReceiver {
 
-    protected Alarm alram;
+    protected Alarm alarm;
+
+    public ChargingReceiver() {
+        this.alarm = null;
+    }
 
     public ChargingReceiver(Alarm alarm) {
-        this.alram = alarm;
+        this.alarm = alarm;
     }
 
     public void onReceive(Context context, Intent intent) {
+        if (this.alarm == null) return;
+
         String action = intent.getAction();
 
-        if(action.equals(Intent.ACTION_POWER_CONNECTED)) {
-            alram.playAlarm();
-        }
-        else if(action.equals(Intent.ACTION_POWER_DISCONNECTED)) {
+        if (action.equals(Intent.ACTION_POWER_CONNECTED)) {
+            alarm.playAlarm();
+        } else if (action.equals(Intent.ACTION_POWER_DISCONNECTED)) {
 
         }
     }
